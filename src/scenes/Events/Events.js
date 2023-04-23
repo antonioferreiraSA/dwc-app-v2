3de17e51dcccc57f9bdb24c30e5619cb0a52b1f1
antonio <antonio@ovex.io>
2023-04-23 17:28:21 +0200

    timer

--- src/scenes/Events/Events.js
+++ src/scenes/Events/Events.js
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { SafeAreaView } from 'react-native';
 import WebView from 'react-native-webview';
 
 const Events = () => {
-  const [uri, setUri] = useState('https://mobile.destinyworshipcentre.co.za/events-2/');
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setUri(uri => uri); // This triggers a re-render and refreshes the WebView
-    }, 60000); // Refresh every 5 seconds
-
-    return () => clearInterval(intervalId); // Clean up the interval when the component unmounts
-  }, []);
+  const uri = 'https://mobile.destinyworshipcentre.co.za/events-2/';
 
   return (
     <SafeAreaView style={{flex:1}}>
